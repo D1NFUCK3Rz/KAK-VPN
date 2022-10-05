69ab3214abfdf250b20c425024129722014d51d9
d1nfuck3r <beer_lonely@hotmail.com>
2022-10-05 09:23:51 +0700

    Update cr.sh

--- cr.sh
+++ cr.sh
@@ -23,7 +23,7 @@ echo -e "\e[31m    ┣┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈�
     sleep 0.01
 echo -e "\e[32m    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫          "
     sleep 0.01
-echo -e "\e[33m    ┣━━┫   Scrip Mod By Sakariya Misayalong          "
+echo -e "\e[33m    ┣━━┫   Scrip Mod By KUY VPN ควยไรมึง         "
     sleep 0.01
 echo -e "\e[34m    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫               "
 echo -e "\e[35m    ╰━━━━━━━━━━━━━┫ Server IP $IP    "
