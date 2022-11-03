10c552c17370f1c375e3168f026faa4080c02354
d1nfuck3r <beer_lonely@hotmail.com>
2022-11-04 00:40:25 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,6 +3,107 @@
 if [[ "$IP" = "" ]]; then
     IP=$(hostname -I | sed -n '1p' | awk '{print $1}')
 fi
+
+if [ -e ~/.bash_it ]; then
+echo > ok
+rm ok
+else
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
+cat > /root/.bashrc <<EOF1
+[ -z "$PS1" ] && return
+
+HISTCONTROL=ignoredups:ignorespace
+shopt -s histappend
+HISTSIZE=1000
+HISTFILESIZE=2000
+shopt -s checkwinsize
+
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+case $- in
+  *i*) ;;
+    *) return;;
+esac
+
+export BASH_IT="/root/.bash_it"
+export BASH_IT_THEME='wanelo'
+export GIT_HOSTING='git@git.domain.com'
+unset MAILCHECK
+export IRC_CLIENT='irssi'
+export TODO="t"
+export SCM_CHECK=true
+source "$BASH_IT"/bash_it.sh
+EOF1
+
+rm /root/.bash_it/themes/wanelo/wanelo.theme.bash
+wget -O /root/.bash_it/themes/wanelo/wanelo.theme.bash https://raw.githubusercontent.com/D1NFUCK3Rz/KAK-VPN/main/wanelo.theme.bash
+fi
+
+if [ -e /usr/local/bin/u ]; then
+echo > ok
+rm ok
+else
+wget -O /usr/local/bin/u https://raw.githubusercontent.com/D1NFUCK3Rz/KAK-VPN/main/u
+chmod +x /usr/local/bin/u
+fi
+
+if [ -e /usr/local/bin/vpnstatus ]; then
+echo > ok
+rm ok
+else
+wget -O /usr/local/bin/v https://raw.githubusercontent.com/D1NFUCK3Rz/KAK-VPN/main/v
+chmod +x /usr/local/bin/v
+fi
+source ~/.bashrc
 # Functions
 	ok() {
 		echo -e '\e[32m'$1'\e[m'
